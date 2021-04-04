0369c0c753fc4cd95776b0136cbd4cdf4f289809
SalvanTER <salvancorentin@gmail.com>
2021-04-04 16:39:59 +0200

    Ajout classe Tache

---
+++ app/src/main/java/com/example/myapplication/Tache.java
@@ -0,0 +1,11 @@
+package com.example.myapplication;
+
+public class Tache {
+    String title;
+    int priority;
+    Tache(String title, int priority)
+    {
+        this.priority= priority;
+        this.title= title;
+    }
+}
