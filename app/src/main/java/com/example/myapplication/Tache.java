52d600b2f339f03593360bcc9a1f8cccf4bf50d4
SalvanTER <salvancorentin@gmail.com>
2021-04-06 12:37:14 +0200

    Ajout attribut nbDay à Tache

--- app/src/main/java/com/example/myapplication/Tache.java
+++ app/src/main/java/com/example/myapplication/Tache.java
@@ -3,9 +3,11 @@ package com.example.myapplication;
 public class Tache {
     String title;
     int priority;
-    Tache(String title, int priority)
+    int nbDay;
+    Tache(String title, int priority, int nbDay)
     {
         this.priority= priority;
         this.title= title;
+        this.nbDay= nbDay;
     }
 }
