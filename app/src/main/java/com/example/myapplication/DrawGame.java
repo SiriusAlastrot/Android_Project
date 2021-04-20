4fab77b7b11b18c0a80cb314fd2accc4256c3d29
SalvanTER <salvancorentin@gmail.com>
2021-04-20 07:07:03 +0200

    Ajout de la classe DrawGame

---
+++ app/src/main/java/com/example/myapplication/DrawGame.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class DrawGame extends View{
+    public DrawGame(Context context, AttributeSet attrs) {
+        super(context,attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+    }
+}
