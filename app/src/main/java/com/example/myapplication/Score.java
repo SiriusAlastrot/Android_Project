e328ab52007b098013cd747334fcf4dbf7a97353
SiriusAlastrot <medericrococo@orange.fr>
2021-04-22 20:25:27 +0300

    Ajout de la classe score

---
+++ app/src/main/java/com/example/myapplication/Score.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+public class Score {
+    String pseudo;
+    float temps;
+    String niveau;
+
+
+    public Score(String pseudo, float temps, String niveau) {
+        this.pseudo = pseudo;
+        this.temps = temps;
+        this.niveau = niveau;
+
+    }
+}
+
+
